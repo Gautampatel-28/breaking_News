41001a1075fe74bad9d8a5e9f5d269404fa931cf
Gautampatel-28 <gautampatel7233@gmail.com>
2024-10-01 11:26:36 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/breaking_News/', 
+});
